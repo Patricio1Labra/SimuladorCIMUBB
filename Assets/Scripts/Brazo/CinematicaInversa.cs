8db8c4b64155c8887622561c6cf29982ada0e587
Patricio1Labra <patricio.labra1801@alumnos.ubiobio.cl>
2023-07-06 18:26:44 -0400

    Prueba de nuevos metodos

--- Assets/Scripts/Brazo/CinematicaInversa.cs
+++ Assets/Scripts/Brazo/CinematicaInversa.cs
@@ -4,15 +4,151 @@ using UnityEngine;
 
 public class CinematicaInversa : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variables de bones, huesos y punto de objetivo
+     //[] Caja que guarda todas las bones
+    [SerializeField]
+    Transform[] bones; //De tipo transform porque hablamos de posición, rotación y escala del objeto.
+    float[] bonesLengths; //Guardamos longitudes (escala) de los huesos
+    
+    [SerializeField]
+    Transform targetPosition;
+    
+    [SerializeField]
+    Transform limitador; //Limitra el ángulo del desplazamiento del codo de nuestro brazo
+    
+    //Variables del metodo FABRIK (iteracion)
+    [SerializeField]
+    int solverIterations = 5; //En una iteracion aplica el método completo, se hacen 5 para que
+                              //sea más exacto el movimiento
+
+    private void Start()
     {
-        
+        //Se calculan las longitudes de los huesos para ser tomadas en el metodo FABRIK para
+        //que tenga sentido y no parezcan que se parten.
+        bonesLengths = new float[bones.Length];
+
+        //En un for analizamos la longitud de cada hueso en caso de que sea el ultimo hueso
+        //entonces la longitud será 0 porque no existe hueso en la ultima articulacion
+        for(int i = 0; i < bones.Length; i++)
+        {
+            if(i < bones.Length - 1)
+            {
+                //Aquí va recorriendo hueso por hueso y se obtiene la manitud entre bones
+                //que dan la longitud de los huesos
+                bonesLengths[i] = (bones[i + 1].position - bones[i].position).magnitude;
+            }
+            else
+            {
+                bonesLengths[i] = 0f;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        SolveIK();
     }
+
+    //MÉTODO FABRIK
+    // Nota:Algoritmo que clacula posiciones pero no las rotaciones
+    //Invertir el orden en que se analiza las posiciones, es decir, comenzando del Objetivo a la articulación 1 (raíz)
+    private Vector3[] SolveInversePositions (Vector3[] forwardPositions)
+    {
+        Vector3[] inversePositions = new Vector3[forwardPositions.Length]; //Almacenamiento
+
+        //se calcula las posiciones ideales desde el último hasta el primer hueso en base las posiciones actuales
+        for (int i = (forwardPositions.Length - 1); i >= 0; i--)
+        {
+            if(i == forwardPositions.Length- 1)
+            {
+                //Si es el último hueso, la posición prima es la misma que la posición objetivo
+                inversePositions[i]= targetPosition.position;
+            }
+            else
+            {
+                Vector3 posPrimaSiguiente = inversePositions[i+ 1];
+                Vector3 posBaseActual =forwardPositions[i];
+                Vector3 direccion = (posBaseActual - posPrimaSiguiente).normalized; //Almacena un vector unitario
+                                                                                    //desde posPrimaSiguiente
+                float longitud = bonesLengths[i];
+                inversePositions[i] = posPrimaSiguiente + (direccion * longitud);
+            }
+        }
+        return inversePositions;
+    }
+
+    private Vector3[] SolveForwardPositions (Vector3[] inversePositions)
+    {
+        Vector3[] forwardPositions = new Vector3[inversePositions.Length];
+        //Calculamos las posiciones 'reales' desde el primer hasta el último hueso en base a las posiciones 'ideales'
+        for (int i = 0; i < inversePositions.Length; i++)
+        {
+            if (i == 0)
+            {
+                //Si es el primer hueso, la posición es la misma que la posición del primer hueso base
+                forwardPositions[i] = bones[0].position;
+            }
+            else
+            {
+                Vector3 posPrimaActual = inversePositions[i];
+                Vector3 posPrimaSegundaAnterior = forwardPositions[i - 1];
+                Vector3 direccion = (posPrimaActual - forwardPositions[i - 1]).normalized;   //Almacena un vector unitario
+                                                                                            //desde posPrimaSegundaAnterior hasta
+                                                                                            //posPrimaActual y normalizamos
+                float longitud = bonesLengths[i - 1];//Long del hueso anterior
+                forwardPositions[i] = posPrimaSegundaAnterior + (direccion * longitud);
+            }
+        }
+        return forwardPositions;
+    }
+
+    private void SolveIK()
+    {
+        Vector3[] finalBonesPositions = new Vector3[bones.Length];
+
+        //Guardamos las posiciones actuales de los huesos en una lista de posiscionesFinalesHuesos
+        for(int i = 0; i < bones.Length; i++)
+        {
+            finalBonesPositions[i] = bones[i].position;
+        }
+
+        //Itertamos el Método FABRIK tantas veces como la variable solverIteration tenga
+        for (int i = 0; i < solverIterations; i++)
+        {
+            //Calculamos valores primos en base a las posiciones actuales de los huesos y con el
+            //resultado de esta función se los pasaremos a la función que calcula los primos segundos
+            //у estas serán las posiciones resultantes a guardar en la variable de posiciones finales
+            finalBonesPositions = SolveForwardPositions(SolveInversePositions(finalBonesPositions));
+        }
+            
+        //Tras iterar el método le asignamos las posiciones obtenidas a cada hueso y calculamos las
+        //rotaciones para que cada hueso apunte al siguiente
+        for (int i = 0; i < bones.Length; i++)
+        {
+            bones[i].position = finalBonesPositions[i];
+
+            //Aplicamos rotaciones
+            if(i != bones.Length - 1)
+            {
+                bones[i].rotation = Quaternion.LookRotation(finalBonesPositions[i + 1] - bones[i].position);
+            }
+            else
+            {
+                bones[i].rotation = Quaternion.LookRotation(targetPosition.position - bones[i].position);
+            }
+        }
+
+        /* if(limitador != null)
+        {
+            for (int i = 1; i < bones.Length - 1; i++)
+            {
+                Plane projectionPlane = new Plane(bones[i + 1].position - bones[i - 1].position, bones[i - 1].position);
+                Vector3 projectedBonePosition = projectionPlane.ClosestPointOnPlane(bones[i].position);
+                Vector3 projectedPole = projectionPlane.ClosestPointOnPlane(limitador.position);
+                float angleOnPlane = Vector3.SignedAngle(projectedBonePosition - bones[i - 1].position, projectedPole - bones[i - 1].position, bones[i - 1].position);
+                bones[i].position = Quaternion.AngleAxis(angleOnPlane, projectionPlane.normal) * (bones[i].position - bones[i - 1].position);
+            }
+        } */
+    }
+
 }
--- Assets/Scripts/Brazo/MovimientoXYZ.cs
+++ Assets/Scripts/Brazo/MovimientoXYZ.cs
@@ -4,15 +4,126 @@ using UnityEngine;
 
 public class MovimientoXYZ : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform EndEffector; // Transform del efector final
+    public float velocidadMovimiento = 5f; // Velocidad de movimiento
+    public KeyCode teclaPositivaX = KeyCode.Q; // Tecla para moverse en dirección positiva en el eje X
+    public KeyCode teclaNegativaX = KeyCode.A; // Tecla para moverse en dirección negativa en el eje X
+    public KeyCode teclaPositivaY = KeyCode.W; // Tecla para moverse en dirección positiva en el eje Y
+    public KeyCode teclaNegativaY = KeyCode.S; // Tecla para moverse en dirección negativa en el eje Y
+    public KeyCode teclaPositivaZ = KeyCode.E; // Tecla para moverse en dirección positiva en el eje Z
+    public KeyCode teclaNegativaZ = KeyCode.D; // Tecla para moverse en dirección negativa en el eje Z
+    private Vector3 posicionAnterior;   
+
+    //public Transform base; // Referencia al objeto base (primera articulación)
+    public Transform shoulder; // Referencia al objeto shoulder (segunda articulación)
+    public Transform elbow; // Referencia al objeto elbow (tercera articulación)
+    public Transform wrist; // Referencia al objeto Wrist
+    private float baseLenght;
+    private float shoulderLenght;
+    private float elbowLenght;
+    private float wristLenght;
+
     void Start()
     {
-        
+        posicionAnterior = EndEffector.position;
+        SetLenghts();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MoverEndEffector();
+    }
+
+    private void MoverEndEffector()
+    {
+        float movimientoHorizontal = 0f;
+        float movimientoVertical = 0f;
+        float movimientoProfundidad = 0f;
+
+        if (!Input.anyKeyDown)
+        {
+            //return;
+        }
+        // Verificar las teclas presionadas y asignar los valores correspondientes
+        if (Input.GetKey(teclaPositivaX))
+        {
+            movimientoHorizontal += 1f;
+        }
+        if (Input.GetKey(teclaNegativaX))
+        {
+            movimientoHorizontal -= 1f;
+        }
+        if (Input.GetKey(teclaPositivaY))
+        {
+            movimientoVertical += 1f;
+        }
+        if (Input.GetKey(teclaNegativaY))
+        {
+            movimientoVertical -= 1f;
+        }
+        if (Input.GetKey(teclaPositivaZ))
+        {
+            movimientoProfundidad += 1f;
+        }
+        if (Input.GetKey(teclaNegativaZ))
+        {
+            movimientoProfundidad -= 1f;
+        }
+
+        // Calcular el desplazamiento del endeffector en los tres ejes
+        Vector3 desplazamiento = new Vector3(movimientoHorizontal, movimientoVertical, movimientoProfundidad) * velocidadMovimiento * Time.deltaTime;
+
+        // Aplicar el desplazamiento al endeffector
+        EndEffector.position += desplazamiento;
+
+        Vector3 nuevaPosicion = EndEffector.position;
+
+        // Verificar si la posición es la misma que la anterior
+        if (nuevaPosicion == posicionAnterior)
+        {
+            // La posición es la misma, no hacer nada
+            return;
+        }
+
+        // La posición es diferente, guardar la nueva posición
+        posicionAnterior = nuevaPosicion;
+
+        // Realizar la cinemática inversa para actualizar las articulaciones
+        //RealizarCinematicaInversa();
+        //SetDirections();
+    }
+
+    private void RealizarCinematicaInversa()
+    {
         
+    }
+
+    private void SetLenghts()
+    {
+        shoulderLenght = (elbow.position - shoulder.position).magnitude;
+        elbowLenght = (EndEffector.position - elbow.position).magnitude;
+    }
+
+    private void SetDirections()
+    {
+        float shoulderToWrist = (wrist.position - shoulder.position).magnitude;
+
+        float shoulderAngle = GetAngle(shoulderLenght, shoulderToWrist, elbowLenght);
+        float elbowAngle = GetAngle(elbowLenght, shoulderLenght,shoulderToWrist);
+
+        if (!float.IsNaN(shoulderAngle) && !float.IsNaN(elbowAngle))
+        {
+            Quaternion shoulderRot = Quaternion.AngleAxis(-shoulderAngle,Vector3.forward);
+            shoulder.localRotation = shoulderRot;
+
+            Quaternion elbowRot = Quaternion.AngleAxis(180 - elbowAngle,Vector3.forward);
+            elbow.localRotation = elbowRot;
+        }
+    }
+
+    private float GetAngle (float a, float b, float c)
+    {
+        return Mathf.Acos((a * a + b * b - c * c) / (2 * a * b)) * Mathf.Rad2Deg;
     }
 }
