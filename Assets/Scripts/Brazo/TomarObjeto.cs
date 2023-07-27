80d4a34fc0f1f957ed83ca418798e88490b85944
Patricio1Labra <patricio.labra1801@alumnos.ubiobio.cl>
2023-07-27 12:11:25 -0400

    tomar y solar objeto, teclado y boton

---
+++ Assets/Scripts/Brazo/TomarObjeto.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TomarObjeto : MonoBehaviour
+{
+    public GameObject handPoint;
+    private GameObject pickedObject = null;
+    private bool podertomar = false;
+    private bool boton = false;
+    private bool tomado = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pickedObject != null)
+        {
+            if(Input.GetKey("x") || !boton)
+            {
+                pickedObject.GetComponent<Rigidbody>().useGravity = true;
+                pickedObject.GetComponent<Rigidbody>().isKinematic = false;
+                pickedObject.gameObject.transform.SetParent(null);
+                pickedObject = null;
+                tomado = false;
+            }
+            
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Objeto"))
+        {
+            if(pickedObject == null)
+            {
+                podertomar = true;
+                if(Input.GetKey("z") || boton)
+                {
+                    other.GetComponent<Rigidbody>().useGravity = false;
+                    other.GetComponent<Rigidbody>().isKinematic = true;
+                    other.transform.position = handPoint.transform.position;
+                    other.gameObject.transform.SetParent(handPoint.gameObject.transform);
+                    pickedObject = other.gameObject;
+                    tomado = true;
+                    podertomar = false;
+                }
+                
+            }
+        }
+        
+    }
+
+    public void OnClick()
+    {
+        if(podertomar)
+        {
+            boton = true;
+        }
+        if(tomado)
+        {
+            boton = false;
+        }
+        
+    }
+}
